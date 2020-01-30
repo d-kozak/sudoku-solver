44237f4cae9615e8978c1ab9511de9723db26f74
dkozak <dkozak94@gmail.com>
2020-01-30 17:05:49 +0100

    make the solver a bit smarter
    still does not solve the difficult example though :X

--- src/main/kotlin/io/dkozak/sudoku/solver/model/SudokuRem.kt
+++ src/main/kotlin/io/dkozak/sudoku/solver/model/SudokuRem.kt
@@ -1,5 +1,6 @@
 package io.dkozak.sudoku.solver.model
 
+import mu.KotlinLogging
 import java.io.File
 import java.util.*
 import kotlin.math.sqrt
@@ -23,46 +24,75 @@ fun loadSudokuRem(path: String): SudokuRem {
             if (cell != ' ') sudoku[i, j] = cell - '0'
         }
     }
-    sudoku.validate()
+    sudoku.validate(true)
     return sudoku
 }
 
 fun solve(puzzle: SudokuRem): SudokuRem {
     val queue: Queue<Triple<Int, Int, Int>> = LinkedList()
+    val numSlots = Array(puzzle.size) { -1 to -1 }
+
+    fun processCell(row: Int, col: Int, cell: SudokuCell) {
+        if (cell.finalValue != -1) return
+        val options = cell.options
+        if (options.size == 1) {
+            val option = options.first()
+            queue.add(Triple(row, col, option))
+            numSlots[option - 1] = -2 to -2
+        } else for (option in options) {
+            if (numSlots[option - 1].first == -1)
+                numSlots[option - 1] = row to col
+            else if (numSlots[option - 1].first >= 0)
+                numSlots[option - 1] = -2 to -2
+        }
+    }
+
+    fun processNumSlots() {
+        for ((i, slot) in numSlots.withIndex()) {
+            if (numSlots[i].first >= 0) {
+                queue.add(Triple(slot.first, slot.second, i + 1))
+            }
+            numSlots[i] = -1 to -1
+        }
+    }
 
     puzzle.forEachCellIndexed { row, col, cell ->
+        if (row == 0) {
+            puzzle.forColIndexed(col, ::processCell)
+            processNumSlots()
+        }
+        if (col == 0) {
+            puzzle.forRowIndexed(row, ::processCell)
+            processNumSlots()
+        }
+
+        if (row % puzzle.regionSize == 0 && col % puzzle.regionSize == 0) {
+            puzzle.forRegionIndexed(row, col, ::processCell)
+            processNumSlots()
+        }
+
         val option = cell.value
         if (option != null) {
             queue.add(Triple(row, col, option))
         }
     }
 
-
     while (queue.isNotEmpty()) {
         val (row, col, value) = queue.poll()
+        if (puzzle[row, col].finalValue != -1) continue
         puzzle[row, col] = value
-        puzzle.forRowIndexed(row) { row, col, cell ->
-            val option = cell.value
-            if (option != null) {
-                queue.add(Triple(row, col, option))
-            }
-        }
-        puzzle.forColIndexed(col) { row, col, cell ->
-            val option = cell.value
-            if (option != null) {
-                queue.add(Triple(row, col, option))
-            }
-        }
 
-        puzzle.forRegionIndexed(row, col) { row, col, cell ->
-            val option = cell.value
-            if (option != null) {
-                queue.add(Triple(row, col, option))
-            }
-        }
+        puzzle.forRowIndexed(row, ::processCell)
+        processNumSlots()
+
+        puzzle.forColIndexed(col, ::processCell)
+        processNumSlots()
+
+        puzzle.forRegionIndexed(row, col, ::processCell)
+        processNumSlots()
     }
 
-    puzzle.validate()
+    puzzle.validate(false)
     println(puzzle.toSudokuPuzzle())
     return puzzle
 }
@@ -111,6 +141,8 @@ data class SudokuCell(val content: BitSet, var finalValue: Int = -1) {
         }
 }
 
+private val logger = KotlinLogging.logger { }
+
 class SudokuRem(val content: Array<Array<SudokuCell>>) {
 
     constructor(size: Int) : this(Array(size) { Array(size) { SudokuCell.empty(size) } })
@@ -124,12 +156,12 @@ class SudokuRem(val content: Array<Array<SudokuCell>>) {
         check(regionSize.toInt().toDouble() == regionSize) { "$size cannot be squared exactly, result was $regionSize" }
         this.regionSize = regionSize.toInt()
 
-        validate()
+        validate(true)
     }
 
-    fun validate() {
-        val errors = SudokuValidator(this).validate()
-        if (errors.isNotEmpty()) throw IllegalStateException(errors.toString())
+    fun validate(allowEmptyCell: Boolean) {
+        val errors = SudokuValidator(this, allowEmptyCell).validate()
+        if (errors.isNotEmpty()) throw IllegalStateException(this.toSudokuPuzzle().toString() + "\n" + errors.joinToString("\n"))
     }
 
 
@@ -145,11 +177,15 @@ class SudokuRem(val content: Array<Array<SudokuCell>>) {
         }
     }
 
+    operator fun get(row: Int, col: Int) = content[row][col]
+
     operator fun set(row: Int, col: Int, value: Int) {
+        logger.info { "Setting [$row][$col] to $value" }
         forRow(row) { it.content.clear(value - 1) }
         forCol(col) { it.content.clear(value - 1) }
         forRegion(row, col) { it.content.clear(value - 1) }
         content[row][col].set(value)
+        validate(true)
     }
 
     fun forRow(row: Int, block: (SudokuCell) -> Unit) {
@@ -189,45 +225,42 @@ class SudokuRem(val content: Array<Array<SudokuCell>>) {
 
 }
 
-private class SudokuValidator(val puzzle: SudokuRem) {
-    val errors = mutableSetOf<String>()
+private class SudokuValidator(val puzzle: SudokuRem, val allowEmptyCell: Boolean = false) {
+    val errors = mutableListOf<String>()
 
     fun isValid(): Boolean = validate().isEmpty()
 
     fun validateSequence(prefix: String, callback: ((Int, Int, SudokuCell) -> Unit) -> Unit) {
         val numLocations = mutableMapOf<Int, MutableSet<Pair<Int, Int>>>()
         callback { i, j, cell ->
-            val options = cell.options
-            for (option in options) {
-                val numOptions = numLocations.computeIfAbsent(option) { mutableSetOf() }
-                numOptions.add(i to j)
-            }
-            if (options.isEmpty()) {
-                errors.add("$prefix: [$i][$j] : cell has no options")
+            if (cell.finalValue == -1 && !allowEmptyCell) {
+                errors.add("$prefix: [$i][$j] is empty")
+            } else if (cell.finalValue != -1) {
+                numLocations.computeIfAbsent(cell.finalValue) { mutableSetOf() }
+                        .add(i to j)
             }
         }
 
         for ((num, locations) in numLocations) {
             if (locations.size > 1)
-                errors.add("$prefix: Number $num is present in multiple cells $locations")
+                errors.add("$prefix: Number ${num + 1} is present in multiple cells $locations")
         }
     }
 
 
     fun validate(): List<String> {
-        val errors = mutableListOf<String>()
         for ((i, row) in puzzle.content.withIndex()) {
             if (row.size != puzzle.size) errors.add("Row $i does not have expected size ${puzzle.size}, it has ${row.size}")
         }
 
         for (i in 0 until puzzle.size) {
-            validateSequence("Row $i") { block -> puzzle.forRowIndexed(i, block) }
-            validateSequence("Col $i") { block -> puzzle.forColIndexed(i, block) }
+            validateSequence("Row ${i + 1}") { block -> puzzle.forRowIndexed(i, block) }
+            validateSequence("Col ${i + 1}") { block -> puzzle.forColIndexed(i, block) }
         }
 
         for (i in 0 until puzzle.size step puzzle.regionSize)
             for (j in 0 until puzzle.size step puzzle.regionSize)
-                validateSequence("Region [$i..${i + puzzle.regionSize}][$j..${j + puzzle.regionSize}]") { block -> puzzle.forRegionIndexed(i, j, block) }
+                validateSequence("Region [${i + 1}..${i + puzzle.regionSize}][${j + 1}..${j + puzzle.regionSize}]") { block -> puzzle.forRegionIndexed(i, j, block) }
 
         return errors
     }
